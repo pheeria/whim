1cc7f8e36bc7a8776e5aae00d466d97c8bbfcb50
pheeria <olzhas.askar@gmail.com>
2019-03-11 22:22:19 +0100

    Use axios and unsplash

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const { unsplash } = require("./unsplash");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Whim!");
+app.get("/:word", (req, res) => {
+  unsplash.get(`search/photos?query=${req.params.word}`).then(response => {
+    res.send(response.data);
+  });
 });
 
 app.listen(4200, () => console.log("Listening on port 4200!"));
