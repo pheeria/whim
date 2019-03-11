b6b7274b0f3d31813b67d24fbb8d69470e2307dc
pheeria <olzhas.askar@gmail.com>
2019-03-11 21:32:58 +0100

    Add index

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Whim!');
+});
+
+app.listen(4200, () => console.log('Listening on port 4200!'));
