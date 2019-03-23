088c883ef978de85a3e96fabc5d7f5d2b66cb41a
Olzhas Askar <olzhas.askar@gmail.com>
2019-03-23 01:25:57 +0100

    Create layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
+import { Button, Box, Grid, Grommet, Text } from "grommet";
+import { CirclePlay } from "grommet-icons";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            whim!
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Grommet plain full>
+    <Grid fill gap="small" rows={["auto", "flex", "auto"]}>
+      <Grid fill columns={["auto", "flex", "auto"]}>
+        <Text alignSelf="start">happy</Text>
+        <Text> </Text>
+        <Text alignSelf="end">14</Text>
+      </Grid>
+
+      <Grid
+        rows={["auto", "auto"]}
+        columns={["auto", "auto"]}
+        gap="medium"
+        areas={[
+          { name: "a", start: [0, 0], end: [1, 0] },
+          { name: "b", start: [1, 0], end: [1, 1] },
+          { name: "x", start: [0, 1], end: [0, 1] },
+          { name: "y", start: [1, 1], end: [1, 1] }
+        ]}
+      >
+        <Box gridArea="a" background="brand">
+          a
+        </Box>
+        <Box gridArea="b" background="accent-1">
+          b
+        </Box>
+        <Box gridArea="x" background="accent-2">
+          x
+        </Box>
+        <Box gridArea="y" background="accent-4">
+          y
+        </Box>
+      </Grid>
+
+      <Button icon={<CirclePlay />} primary label="Check" onClick={() => {}} />
+    </Grid>
+  </Grommet>
+);
 
 export default App;
