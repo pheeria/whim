fbb3a0ca73ae21345d7a57f4d6662e354f84c413
Olzhas Askar <olzhas.askar@gmail.com>
2019-03-23 19:28:57 +0100

    Add next endpoint

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.get("/", async (req, res) => {
   while (true) {
     const word = getRandomWord();
     imgs.push(`<h1>${word}</h1>`);
-      const fromUnsplash = await unsplash.get(`search/photos?query=${word}`);
+    const fromUnsplash = await unsplash.get(`search/photos?query=${word}`);
 
     if (fromUnsplash.data.total) {
       fromUnsplash.data.results.forEach(element => {
@@ -34,16 +34,34 @@ app.get("/", async (req, res) => {
   }
 });
 
+app.get("/next", async (req, res) => {
+  const result = { options: [], keyword: "", answer: "" };
+  while (true) {
+    const word = getRandomWord();
+    result.keyword = word;
+    result.answer = 0;
+    const fromUnsplash = await unsplash.get(`search/photos?query=${word}`);
+
+    if (fromUnsplash.data.total > 3) {
+      for (let i = 0; i < 4; i++) {
+        let picture = fromUnsplash.data.results[i];
+        result.options.push(picture.urls.small);
+      }
+      res.send(result);
+    }
+  }
+});
+
 app.get("/:word", (req, res) => {
   unsplash.get(`search/photos?query=${req.params.word}`).then(response => {
-      const imgs = [];
-      response.data.results.forEach(element => {
-        imgs.push(
-          `<img src="${element.urls.small}" title="By ${element.user.name}"/>`
-        );
-      });
-      res.send(imgs.join(" "));
+    const imgs = [];
+    response.data.results.forEach(element => {
+      imgs.push(
+        `<img src="${element.urls.small}" title="By ${element.user.name}"/>`
+      );
     });
+    res.send(imgs.join(" "));
+  });
 });
 
 app.listen(4200, () => console.log("Listening on port 4200!"));
